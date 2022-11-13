c76d9dc8d51b1697bf677f12e2600e193a23172c
shadoww <dev.shadoww@gmail.com>
2022-11-13 23:17:40 +0530

    Refactor code and update README.md

--- README.md
+++ README.md
@@ -1,49 +1,29 @@
-# BE SORT :
+# BE Sort
 
-`BE SORT` is an sorting algorithm and implemented using C Language.
+A sorting algorithm inspired by **quick sort**.This program is created in **C** language.
 
----
+## How it works ?
 
-## WHY I CREATED IT ?
+There are two pointers (Not actual C pointers, just normal variables) `pointElementI` and `pointElementJ`, it works in two stages, in first stage the `pointElementI` is fixed and in second stage `pointElementJ` is fixed, but in both stages `i` is incremented and `j` is decremented.
 
-When i was studying `quick sort` it blew my mind. I don't know whether this algorithm is already been created but this idea just came to my mind and i implemented it with my favorite tool / programming language C. At first i had a different approch but over the course of implementation of algorithm it changed. I already created another sorting program know as `MAXDELETE SORT` if you haven't seen it check it out [here](https://github.com/dev-shadoww/maxDeleteSort).
+To explain this i have created these 👇, the **red** colored star indicates a new stage within the main stage, at this stage `i` again points to it's initial value, but `j` is decremented in first stage and vice versa.
 
----
+### Stage One
 
-## HOW IT WORKS ?
+![besort_1](images/besort_1.png)
 
-It has two stages `IMAGE 1` and `IMAGE 2` explains the first stage while `IMAGE 3` and `IMAGE 4` explains the second stage and finally in `IMAGE 5` we have our sorted list.
+![besort_2](images/besort_2.png)
 
-At first **i** is pointing to 0 and **j** is pointing two **n - 1** and we start compairing **i**th element with **j**th element if **i**th element is less than the **j**th element then we swap the elements and **i** is incremented by one and **j** is decremented by one and this process continues until all the comparisons are done.Then again **i** points to 0 and **j** remains fixed and the above process is repeated until **i** and **j** becomes equal.You can visualize it in `IMAGE 1` and `IMAGE 2`. This is the first stage.
+![besort_3](images/besort_3.png)
 
-###### IMAGE 1 :
+### Stage Two
 
-![IMAGE 1](Images/1.png)
+![besort_4](images/besort_4.png)
 
-###### IMAGE 2 :
+![besort_5](images/besort_5.png)
 
-![IMAGE 2](Images/2.png)
+![besort_6](images/besort_6.png)
 
-In second stage at first **i** is pointing to 0 and **j** is pointing two **n - 1** and we start compairing **i**th element with **j**th element if **i**th element is greater than the **j**th element then we swap the elements and **i** is incremented by one and **j** is decremented by one and this process continues until all the comparisons are done. Then again **j** points to **n - 1** and **i** remains fixed and the above process is repeated until **i** and **j** becomes equal.You can visualize it in `IMAGE 3` and `IMAGE 4`.
+## Output
 
-###### IMAGE 3 :
-
-![IMAGE 3](Images/3.png)
-
-###### IMAGE 4 :
-
-![IMAGE 4](Images/4.png)
-
-Finally we get sorted list.
-
-###### IMAGE 5 :
-
-![IMAGE 5](Images/5.png)
-
-It doesn't use the **i** and **j** values to terminate the loop for that reason it has specific variables. So for more information check out the code.
-
----
-
-## OUTPUT :
-
-![Output](Images/output.png)
+![besort_output](images/besort_output.png)
--- besort.c
+++ besort.c
@@ -1,99 +1,76 @@
 /*
- *   AUTHOR : SHADOWW
- *   LAST MODIFIED : 23 / 06 / 2022
- */
+>>  AUTHOR : SHADOWW
+>>  LAST MODIFIED : 13 / 11 / 2022
+*/
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
 #define MAX 20
 
-int a[MAX], n;
+void besort();
 
-void besortFirst()
-{
-  int i, j, tempElement, repeatElement, indexNumber, countNumber = 0, pointElementI, pointElementJ;
-  repeatElement = (1 + n) / 2;
-  pointElementI = 0;
-  pointElementJ = n - 1;
-  i = pointElementI;
-  j = pointElementJ;
-  while (pointElementI != pointElementJ)
-  {
-    for (indexNumber = 0; indexNumber < repeatElement; indexNumber++)
-    {
-      if (i == j)
-        a[i] = a[j];
-      else if (a[i] > a[j])
-      {
-        tempElement = a[i];
-        a[i] = a[j];
-        a[j] = tempElement;
-      }
-      i++;
-      j--;
-    }
+int arr[MAX], number_of_elements;
+bool stage = false;
 
-    pointElementJ -= 1;
-    i = pointElementI;
-    j = pointElementJ;
-    countNumber += 1;
-    if (countNumber % 2 != 0 && n % 2 != 0) // n IS ODD
-      repeatElement -= 1;
-    else if (countNumber % 2 == 0 && n % 2 == 0) // n IS EVEN
-      repeatElement -= 1;
-  }
-}
-
-void besortSecond()
-{
-  int i, j, tempElement, repeatElement, indexNumber, countNumber = 0, pointElementI, pointElementJ;
-  repeatElement = (1 + n) / 2;
-  pointElementI = 0;
-  pointElementJ = n - 1;
-  i = pointElementI;
-  j = pointElementJ;
-  while (pointElementI != pointElementJ)
-  {
-    for (indexNumber = 0; indexNumber < repeatElement; indexNumber++)
-    {
-      if (i == j)
-        a[i] = a[j];
-      else if (a[i] > a[j])
-      {
-        tempElement = a[i];
-        a[i] = a[j];
-        a[j] = tempElement;
-      }
-      i++;
-      j--;
-    }
-    pointElementI += 1;
-    i = pointElementI;
-    j = pointElementJ;
-    countNumber += 1;
-    if (countNumber % 2 != 0 && n % 2 != 0)
-      repeatElement -= 1;
-    else if (countNumber % 2 == 0 && n % 2 == 0)
-      repeatElement -= 1;
-  }
-}
-
+//////////////////////////////
 int main()
 {
   int i;
-  printf("\n                    -----------\n");
-  printf("                    | BE SORT |\n");
-  printf("                    -----------\n\n");
-  printf("ENTER THE NUMBER OF ELEMENTS : ");
-  scanf("%d", &n);
-  printf("ENTER THE ELEMENTS : ");
-  for (i = 0; i < n; i++)
-    scanf("%d", &a[i]);
-  besortFirst();
-  besortSecond();
-  printf("THE ELEMENTS ARE SORTED SUCCESFULLY : ");
-  for (i = 0; i < n; i++)
-    printf("%d ", a[i]);
+  printf("===========\n");
+  printf("| BE SORT |\n");
+  printf("===========\n");
+  printf("How many numbers 🔢 do you wanna sort ? : ");
+  scanf("%d", &number_of_elements);
+  printf("Ok, got it now please enter the numbers 👇,\n> ");
+  for (i = 0; i < number_of_elements; i++)
+    scanf("%d", &arr[i]);
+  besort();
+  printf("\nHere are the sorted numbers 👇,\n> ");
+  for (i = 0; i < number_of_elements; i++)
+    printf("%d ", arr[i]);
   printf("\n");
   return 0;
+}
+
+//////////////////////////////
+void besort()
+{
+  int i, j, k, tempElement, repeatElement, indexNumber, countNumber = 0, pointElementI, pointElementJ;
+  for (k = 0; k < 2; k++)
+  {
+    countNumber = 0;
+    repeatElement = (1 + number_of_elements) / 2;
+    pointElementI = 0;
+    pointElementJ = number_of_elements - 1;
+    i = pointElementI;
+    j = pointElementJ;
+    while (pointElementI != pointElementJ)
+    {
+      for (indexNumber = 0; indexNumber < repeatElement; indexNumber++)
+      {
+        if (i == j)
+          arr[i] = arr[j];
+        else if (arr[i] > arr[j])
+        {
+          tempElement = arr[i];
+          arr[i] = arr[j];
+          arr[j] = tempElement;
+        }
+        i++;
+        j--;
+      }
+      if (!stage)
+        pointElementJ -= 1;
+      else
+        pointElementI += 1;
+      i = pointElementI;
+      j = pointElementJ;
+      countNumber += 1;
+      if (countNumber % 2 != 0 && number_of_elements % 2 != 0) // n IS ODD
+        repeatElement -= 1;
+      else if (countNumber % 2 == 0 && number_of_elements % 2 == 0) // n IS EVEN
+        repeatElement -= 1;
+    }
+    stage = true;
+  }
 }
